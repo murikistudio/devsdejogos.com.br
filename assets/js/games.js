21dd7dccd6e3efebb5b0401044da918434526806
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2025-04-03 19:10:20 -0300

    adicionar primeiro jogo à listagem

---
+++ assets/js/games.js
@@ -0,0 +1,20 @@
+// Definições de jogos
+const Games = [
+    {
+        name: 'Zombies & Bullets',
+        description: '<strong>Zombies & Bullets</strong> é um jogo de tiro de ação top-down 3D onde você deve ajudar o soldado Jacob a enfrentar exércitos de zumbis em ambientes variados e desafiadores.',
+        images: [
+            'https://murikistudio.com.br/assets/img/zombies-and-bullets/screenshot-0.webp',
+            'https://murikistudio.com.br/assets/img/zombies-and-bullets/screenshot-1.webp',
+            'https://murikistudio.com.br/assets/img/zombies-and-bullets/screenshot-2.webp',
+            'https://murikistudio.com.br/assets/img/zombies-and-bullets/screenshot-3.webp',
+        ],
+        links: [
+            {
+                icon: 'steam.svg',
+                class: 'bg-primary-subtle',
+                url: 'https://store.steampowered.com/app/2802870/Zombies__Bullets/',
+            }
+        ],
+    },
+];
