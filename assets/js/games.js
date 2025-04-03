22e8dbc44bdd524223ed27c1c3bebdbd83c7d014
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2025-04-03 19:35:51 -0300

    atualizar estilos de links

--- assets/js/games.js
+++ assets/js/games.js
@@ -2,6 +2,10 @@
 const Games = [
     {
         name: 'Zombies & Bullets',
+        author: {
+            name: 'Muriki Studio',
+            url: 'https://murikistudio.com.br/',
+        },
         description: '<strong>Zombies & Bullets</strong> é um jogo de tiro de ação top-down 3D onde você deve ajudar o soldado Jacob a enfrentar exércitos de zumbis em ambientes variados e desafiadores.',
         images: [
             'https://murikistudio.com.br/assets/img/zombies-and-bullets/screenshot-0.webp',
@@ -14,7 +18,22 @@ const Games = [
                 icon: 'steam.svg',
                 class: 'bg-primary-subtle',
                 url: 'https://store.steampowered.com/app/2802870/Zombies__Bullets/',
-            }
+            },
+            {
+                icon: 'youtube.svg',
+                class: 'bg-danger',
+                url: 'https://www.youtube.com/@murikistudio',
+            },
+            {
+                icon: 'discord.svg',
+                class: 'bg-primary',
+                url: 'https://discord.gg/f35uuhCvQn',
+            },
+            {
+                icon: 'instagram.svg',
+                class: 'bg-danger',
+                url: 'https://www.instagram.com/murikistudio',
+            },
         ],
     },
 ];
