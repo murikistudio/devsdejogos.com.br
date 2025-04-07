a09f9c21987e64026136a80e7533ae7c5dd1808f
Joel Gomes da Silva <joelgomes1994@users.noreply.github.com>
2025-04-07 00:02:51 -0300

    destacar itens novos na última semana com um emblema

--- assets/js/listing.js
+++ assets/js/listing.js
@@ -184,6 +184,19 @@ createApp({
 
             return filter.label;
         },
+
+        isDateLessThanDaysAgo(dateStr, days) {
+            const date = new Date(dateStr);
+            const now = new Date();
+            const diffMs = now - date;
+            const diffDays = diffMs / (1000 * 60 * 60 * 24);
+            return diffDays < days;
+        },
+
+        formatDate(dateStr) {
+            let date = new Date(dateStr);
+            return date.toLocaleDateString();
+        },
     },
 
     mounted() {
